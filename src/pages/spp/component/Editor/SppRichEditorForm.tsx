88c3b56751eeb4f050380298ebc7e4dd01e2f53c
kanghee2003 <96157340+kanghee2003@users.noreply.github.com>
2026-05-15 11:13:00 +0900

    Update SppRichEditorForm.tsx

--- src/pages/spp/component/Editor/SppRichEditorForm.tsx
+++ src/pages/spp/component/Editor/SppRichEditorForm.tsx
@@ -3,19 +3,28 @@ import React from 'react';
 import { Control, Controller } from 'react-hook-form';
 import SppRichEditor, { SppRichEditorProps } from './SppRichEditor';
 
-export interface RichEditorFormProps extends Omit<SppRichEditorProps, 'defaultHtml' | 'onChangeHtml'> {
+export interface RichEditorFormProps extends Omit<SppRichEditorProps, 'defaultHtml'> {
   name: string;
   control: Control<any>;
   defaultValue?: string;
 }
 
-const SppRichEditorForm = ({ name, control, defaultValue = '', ...props }: RichEditorFormProps) => {
+const SppRichEditorForm = ({ name, control, defaultValue = '', onChangeHtml, ...props }: RichEditorFormProps) => {
   return (
     <Controller
       name={name}
       control={control}
       defaultValue={defaultValue}
-      render={({ field }) => <SppRichEditor {...props} defaultHtml={field.value ?? ''} onChangeHtml={(html: string) => field.onChange(html)} />}
+      render={({ field }) => (
+        <SppRichEditor
+          {...props}
+          defaultHtml={field.value ?? ''}
+          onChangeHtml={(html: string) => {
+            field.onChange(html);
+            onChangeHtml?.(html);
+          }}
+        />
+      )}
     />
   );
 };
