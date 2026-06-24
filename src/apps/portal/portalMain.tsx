8583cc2a1f97c4f5f576c738e92109c6ac67b479
kanghee2003 <96157340+kanghee2003@users.noreply.github.com>
2026-06-24 11:17:44 +0900

    Update portalMain.tsx

--- src/apps/portal/portalMain.tsx
+++ src/apps/portal/portalMain.tsx
@@ -1,5 +1,4 @@
 import PortalApp from '@/apps/portal/PortalApp';
-import SppInputNumber from '@/pages/spp/component/Input/SppInputNumber';
 import { bootstrapApp } from '@/shared/app/AppBootstrap';
 
-bootstrapApp({ app: <SppInputNumber /> });
+bootstrapApp({ app: <PortalApp /> });
